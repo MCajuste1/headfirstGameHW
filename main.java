2ca2aa9fd29978d2f5eeb8f6b22232dfbe62766a
Malcolm Cajuste <malcolmacajuste@gmail.com>
2019-09-13 16:09:21 -0400

    first commit

---
+++ main.java
@@ -0,0 +1,82 @@
+package notes2;
+
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(noparameter());
+        int[] somethingArrays = {3,4,5,6,7};
+        somethingIdk(somethingArrays);
+        int[] arr = {1, 2, 3};
+        somethingIdk(arr);
+
+    }
+
+    public static void somethingIdk(int[] x){
+        for(int z : x ){
+            System.out.println(z);
+        }
+
+    }
+
+
+
+    public static double noparameter(){
+        return 6.6 * 7;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //public static void TentoOne ( int digits){
+
+            //for (int i = digits; i > 0; i--) {
+               // System.out.println(i);
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
